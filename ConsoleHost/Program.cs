f560c9deef1babff1e2e7e356d356a94e6554a57
kandascan <boguslaw.boczkowski@gmail.com>
2017-03-12 22:45:39 +0100

    code refactoring - extend to separate class encrypt PrzekatnoKolumnowy

--- ConsoleHost/Program.cs
+++ ConsoleHost/Program.cs
@@ -4,129 +4,24 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using Szyfry;
 
 namespace ConsoleHost
 {
-    class Key
-    {
-        public char Znak { get; set; }
-        public int OryginalnyIndex { get; set; }
-    }
+    
     class Program
     {
-        static void DrukujTablice(char[,] tablica, string klucz, int wiersze)
-        {
-            var linia = string.Empty;
-
-            var kolumny = klucz.Length;
-            foreach (var key in klucz)
-            {
-                Console.Write("{0} ", key);
-                linia += "--";
-            }
-            Console.WriteLine();
-            Console.WriteLine(linia);
-            for (int i = 0; i < wiersze; i++)
-            {
-                for (int j = 0; j < kolumny; j++)
-                {
-                    Console.Write("{0} ",tablica[i,j]);
-                }
-                Console.WriteLine();
-            }
-        }
-
-        static string SzyfrujTekst(char[,] tablica, string klucz, int wiersze)
-        {
-            List<Key> klucze = new List<Key>();
-            for (int i = 0; i<klucz.Length; i++)
-            {
-                klucze.Add(new Key {OryginalnyIndex = i, Znak = klucz[i]});
-            }
-
-            var sortowaneKlucze = klucze.OrderBy(x => x.Znak).ToArray();
-            
-            var zaszyfrowanyTekst = "";
-
-            var kolumna = 0;
-            var indexKolumny = 0;
-            var dodatkowaZmienna = 1;
-            
-            foreach (var keySort in sortowaneKlucze)
-            {
-                kolumna = keySort.OryginalnyIndex;
-                indexKolumny = kolumna;
-                for (int i = 0; i < wiersze; i++)
-                {
-                    if (kolumna < 0)
-                    {
-                        indexKolumny = klucz.Length - dodatkowaZmienna;
-                        dodatkowaZmienna++;
-                    }
-                    
-                    zaszyfrowanyTekst += tablica[i, indexKolumny];
-                    indexKolumny--;
-                    kolumna --;
-                }
-                dodatkowaZmienna = 1;
-            }
-
-            return zaszyfrowanyTekst;
-        }
-
-        static char[,] UtworzTabliceZTekstu(string tekst, int liczbaKolumn, int liczbaWierszy)
-        {
-            var tablica = new char[liczbaWierszy, liczbaKolumn];
-            var licznikPetli = 0;
-            for (int i = 0; i < liczbaWierszy; i++)
-            {
-                for (int j = 0; j < liczbaKolumn; j++)
-                {
-                    if (licznikPetli >= tekst.Length)
-                    {
-                        tablica[i, j] = 'X';
-                    }
-                    else
-                    {
-                        tablica[i, j] = tekst[licznikPetli];
-                    }
-                    licznikPetli++;
-                }
-            }
-            return tablica;
-        }
-
-        static int ObliczLiczbeWierszy(string tekst, string klucz)
-        {
-            var liczbaWierszy = 0;
-
-            if (tekst.Length % klucz.Length == 0)
-            {
-                liczbaWierszy = tekst.Length / klucz.Length;
-            }
-            else
-            {
-                liczbaWierszy = tekst.Length % klucz.Length + 1;
-            }
-
-            return liczbaWierszy;
-        }
-
         static void Main(string[] args)
         {
             var tekst = "BRYLANTY_SĄ_W_MOJEJ_SKRYTCE_W_BANKU";
             var klucz = "MAROKO";
 
-            var liczbaKolumn = klucz.Length;
-            var liczbaWierszy = ObliczLiczbeWierszy(tekst, klucz);
-            var tablica = UtworzTabliceZTekstu(tekst, liczbaKolumn, liczbaWierszy);
-
-            DrukujTablice(tablica, klucz, liczbaWierszy);
-
-            var zaszyfrowanyTekst = SzyfrujTekst(tablica, klucz, liczbaWierszy);
+            var zaszyfrowany =  PrzekatnoKolumnowy.Szyfruj(tekst, klucz);
+            
             Console.WriteLine();
             Console.WriteLine(tekst);
-            Console.WriteLine(zaszyfrowanyTekst);
+            Console.WriteLine(zaszyfrowany);
+
             Console.ReadLine();
         }
     }
--- Szyfry/PrzekatnoKolumnowy.cs
+++ Szyfry/PrzekatnoKolumnowy.cs
@@ -8,6 +8,119 @@ namespace Szyfry
 {
     public class PrzekatnoKolumnowy
     {
+        private static void DrukujTablice(char[,] tablica, string klucz, int wiersze)
+        {
+            var linia = String.Empty;
 
+            var kolumny = klucz.Length;
+            foreach (var key in klucz)
+            {
+                Console.Write("{0} ", key);
+                linia += "--";
+            }
+            Console.WriteLine();
+            Console.WriteLine(linia);
+            for (int i = 0; i < wiersze; i++)
+            {
+                for (int j = 0; j < kolumny; j++)
+                {
+                    Console.Write("{0} ", tablica[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static string SzyfrujTekst(char[,] tablica, string klucz, int wiersze)
+        {
+            List<Key> key = new List<Key>();
+            for (int i = 0; i < klucz.Length; i++)
+            {
+                key.Add(new Key { OryginalnyIndex = i, Znak = klucz[i] });
+            }
+
+            var sortowaneKlucze = key.OrderBy(x => x.Znak).ToArray();
+
+            var zaszyfrowanyTekst = "";
+
+            var kolumna = 0;
+            var indexKolumny = 0;
+            var iteratorKolumnowy = 1;
+
+            foreach (var keySort in sortowaneKlucze)
+            {
+                kolumna = keySort.OryginalnyIndex;
+                indexKolumny = kolumna;
+                for (int i = 0; i < wiersze; i++)
+                {
+                    if (kolumna < 0)
+                    {
+                        indexKolumny = klucz.Length - iteratorKolumnowy;
+                        iteratorKolumnowy++;
+                    }
+
+                    zaszyfrowanyTekst += tablica[i, indexKolumny];
+                    indexKolumny--;
+                    kolumna--;
+                }
+                iteratorKolumnowy = 1;
+            }
+
+            return zaszyfrowanyTekst;
+        }
+
+        private static char[,] UtworzTabliceZTekstu(string tekst, int liczbaKolumn, int liczbaWierszy)
+        {
+            var tablica = new char[liczbaWierszy, liczbaKolumn];
+            var licznikPetli = 0;
+            for (int i = 0; i < liczbaWierszy; i++)
+            {
+                for (int j = 0; j < liczbaKolumn; j++)
+                {
+                    if (licznikPetli >= tekst.Length)
+                    {
+                        tablica[i, j] = 'X';
+                    }
+                    else
+                    {
+                        tablica[i, j] = tekst[licznikPetli];
+                    }
+                    licznikPetli++;
+                }
+            }
+            return tablica;
+        }
+
+        private static int ObliczLiczbeWierszy(string tekst, string klucz)
+        {
+            var liczbaWierszy = 0;
+
+            if (tekst.Length % klucz.Length == 0)
+            {
+                liczbaWierszy = tekst.Length / klucz.Length;
+            }
+            else
+            {
+                liczbaWierszy = tekst.Length % klucz.Length + 1;
+            }
+
+            return liczbaWierszy;
+        }
+
+        public static string Szyfruj(string tekst, string klucz)
+        {
+            var liczbaKolumn = klucz.Length;
+            var liczbaWierszy = ObliczLiczbeWierszy(tekst, klucz);
+            var tablica = UtworzTabliceZTekstu(tekst, liczbaKolumn, liczbaWierszy);
+            DrukujTablice(tablica, klucz, liczbaWierszy);
+            var zaszyfrowanyTekst = SzyfrujTekst(tablica, klucz, liczbaWierszy);
+
+            return zaszyfrowanyTekst;
+        }
+    }
+
+    class Key
+    {
+        public char Znak { get; set; }
+        public int OryginalnyIndex { get; set; }
     }
 }
