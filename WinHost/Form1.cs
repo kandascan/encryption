c5b3fa14eae79bc351f53e583534d0359a3b6972
kandascan <boguslaw.boczkowski@gmail.com>
2017-03-16 18:57:28 +0100

    implementacja winhost dla przekatno kolumnowego

---
+++ WinHost/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Szyfry;
+
+namespace WinHost
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtZaszyfrowany.Text = PrzekatnoKolumnowy.Szyfruj(txtText.Text, txtKlucz.Text);
+        }
+
+        private void btnOdszyfruj_Click(object sender, EventArgs e)
+        {
+            txtText.Text = PrzekatnoKolumnowy.Odszyfruj(txtZaszyfrowany.Text, txtKlucz.Text);
+        }
+    }
+}
